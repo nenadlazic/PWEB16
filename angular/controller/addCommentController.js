8c27ef753863913deb0319303a34f8dc77009f26
Nenad Lazic <nenadlazic13@gmail.com>
2016-12-11 20:49:33 +0100

    [FRONTEND] Add first frontend version that works

--- angular/controller/addCommentController.js
+++ angular/controller/addCommentController.js
@@ -1,14 +1,14 @@
-<!--na ovaj nacin nasoj web app koja se zove ExploreSerbia definisemo controllere koje koristi(koriscene controllere mozete videti u exploreserbia.js fajlu gde su
- navedeni controlleri i view-i koji se koriste na odgovarajucim putanjama)-->
+//na ovaj nacin nasoj web app koja se zove ExploreSerbia definisemo controllere koje koristi(koriscene controllere mozete videti u exploreserbia.js fajlu gde su
+//navedeni controlleri i view-i koji se koriste na odgovarajucim putanjama)-->
 
 ExploreSerbia.controller('addCommentController',function($scope,$location,$routeParams, commentModel){
 	var offerId = $routeParams.offerId;
-	$scope.cancel() = function(){
+	$scope.cancel = function(){
 		$location.path('/offer/'+offerId);
 	}
 
 	$scope.createComment = function(){
 		commentModel.addComment(offerId, $scope.comment.content);
-		$location.path('/offer'+offerId);
+		$location.path('/offer/'+offerId);
 	}
 });
--- angular/controller/deleteCommentController.js
+++ angular/controller/deleteCommentController.js
@@ -1,11 +1,13 @@
-<!--na ovaj nacin nasoj web app koja se zove ExploreSerbia definisemo controllere koje koristi(koriscene controllere mozete videti u exploreserbia.js fajlu gde su
- navedeni controlleri i view-i koji se koriste na odgovarajucim putanjama)-->
+//na ovaj nacin nasoj web app koja se zove ExploreSerbia definisemo controllere koje koristi(koriscene controllere 
+//mozete videti u exploreserbia.js fajlu gde su
+//navedeni controlleri i view-i koji se koriste na odgovarajucim putanjama)
 
 ExploreSerbia.controller('deleteCommentController',
 	function($scope,$location,$routeParams, commentModel){
 		var offerId = $routeParams.offerId;
 		
+		console.log("brisanje");
 		commentModel.deleteComment(offerId, $routeParams.commentId);
-		$location.path('/offer'+offerId);
+		$location.path('/offer/'+offerId);
 	}
 );
