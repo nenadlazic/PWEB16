20b91f5d7be4bb0e7b70ecb51ab9760d95666356
Djordje Todorovic <djolertrk@gmail.com>
2017-01-22 17:32:11 +0100

    Added folder php/

---
+++ public/php/getComments.php
@@ -0,0 +1,24 @@
+<?php
+	include_once("connect_to_mysql.php");
+
+	$args = json_decode(file_get_contents("php://input"));
+
+	$advert_id = $args->id;
+
+  $sql = "SELECT text, created_at, id FROM comments WHERE advert_id='$advert_id'";
+  $res=mysqli_query($myConnection, $sql);
+  $count = mysqli_num_rows($res);
+  $jsonData = "";
+  if($count>0){
+    while($row=mysqli_fetch_row($res)){
+      $jsonData .= '{"text":"' . $row[0] . '",'
+        . '"date":"' . $row[1] . '",'
+        . '"commentId":' . $row[2] . '"}';
+    }
+  }else{
+    $jsonData = '{"found":false}';
+  }
+
+  echo $jsonData;
+
+?>
---
+++ public/php/getOffers.php
@@ -0,0 +1,26 @@
+<?php
+	include_once("connect_to_mysql.php");
+
+	$args = json_decode(file_get_contents("php://input"));
+
+	$location = $args->city_name;
+	$type = $args->looking_for;
+
+  $sql = "SELECT a.id, a.name, o.description FROM offers o join advertisements a
+    on o.advert_id = a.id WHERE a.location='$location' AND a.tip='$type'";
+  $res=mysqli_query($myConnection, $sql);
+  $count = mysqli_num_rows($res);
+  $jsonData = "";
+  if($count>0){
+    while($row=mysqli_fetch_row($res)){
+      $jsonData .= '{"id":"' . $row[0] . '",'
+        . '"name":"' . $row[1] . '",'
+        . '"description":"' . $row[2] . '"}';
+    }
+  }else{
+    $jsonData = '{"found":false}';
+  }
+
+  echo $jsonData;
+
+?>
