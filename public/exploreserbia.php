2b5255d8d8002f733797a72ac677aba9ba1558cc
Nenad Lazic <nenadlazic13@gmail.com>
2017-01-22 22:59:02 +0100

    Add page for adding new offer when user logged

--- public/exploreserbia.php
+++ public/exploreserbia.php
@@ -1,4 +1,4 @@
-<?php include_once("php/connect_to_mysql.php"); ?>
+
 <!DOCTYPE html>
 <!--pravimo prvi korak u angularu definisemo controler i vidimo kako se koristi-->
 <html ng-app="ExploreSerbia">
@@ -25,6 +25,7 @@
 		<script type="text/javascript" src="controller/addOfferController.js"></script>
 		<script type="text/javascript" src="controller/sendsuccessfulyController.js"></script>
 		<script type="text/javascript" src="controller/contactController.js"></script>
+		<script type="text/javascript" src="controller/addOfferController.js"></script>
 		<script type="text/javascript" src="model/commentModel.js"></script>
 		<script type="text/javascript" src="model/offerModel.js"></script>
 		<script type="text/javascript" src="model/indexModel.js"></script>
