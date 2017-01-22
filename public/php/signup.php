2c5c0aad32ceac4d9016c247a1954a65924f16b3
Zare <zarecacak@gmail.com>
2017-01-22 22:59:28 +0100

    Fixed typo

--- public/php/signup.php
+++ public/php/signup.php
@@ -10,8 +10,8 @@
 	$userType = $args->user_type;
 	if($userType === 'individual'){
 		$sql = "INSERT INTO tourists (email, password, phone, first_name, last_name, points) VALUES('$email', '$password', '$phone', '$fname', '$lname', 0)";
-	}else{
 		$query = mysqli_query($myConnection, $sql) or die(mysqli_error($myConnection));
+	}else{
 		$sql = "INSERT INTO adv_managers (email, phone, first_name, last_name, password, created_at, updated_at)
 						VALUES('$email', '$phone', '$fname', '$lname', '$password', NOW(), NOW())";
 		$query = mysqli_query($myConnection, $sql) or die(mysqli_error($myConnection));
