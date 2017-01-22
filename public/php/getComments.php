ffe69e24288f113e21b4c1b91d2649a90e7e49e8
Zare <zarecacak@gmail.com>
2017-01-22 19:02:31 +0100

    Fix for getComments script json format

--- public/php/getComments.php
+++ public/php/getComments.php
@@ -8,13 +8,17 @@
   $sql = "SELECT text, created_at, id FROM comments WHERE advert_id='$advert_id'";
   $res=mysqli_query($myConnection, $sql);
   $count = mysqli_num_rows($res);
-  $jsonData = "";
+  
+  $jsonData = '{"comments":[';
+  
   if($count>0){
     while($row=mysqli_fetch_row($res)){
       $jsonData .= '{"text":"' . $row[0] . '",'
         . '"date":"' . $row[1] . '",'
-        . '"commentId":' . $row[2] . '"}';
+        . '"commentId":' . $row[2] . '"},';
     }
+    $jsonData = substr(trim($jsonData), 0, -1);
+    $jsonData .= ']}';
   }else{
     $jsonData = '{"found":false}';
   }
